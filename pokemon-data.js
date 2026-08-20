fd39139c5cb44469014adac3c74842d21a995266
VinnieStyle010 <v.a.j.janse@gmail.com>
2026-08-20 18:15:20 +0200

    Update pokemon-data.js

--- pokemon-data.js
+++ pokemon-data.js
@@ -3,18 +3,110 @@ const pokemonData = [
     number: 1,
     name: "Bulbasaur",
     type: ["Grass", "Poison"],
-    game: "Base Game",
+    game: "",
     location: "",
     habitat: "",
     requirements: "",
     dlc: false,
-    tip: "",
-    videos: {
-      youtube: "",
-      tiktok: "",
-      instagram: "",
-      facebook: "",
-      twitter: ""
-    }
+    tip: ""
+  },
+  {
+    number: 2,
+    name: "Ivysaur",
+    type: ["Grass", "Poison"],
+    game: "",
+    location: "",
+    habitat: "",
+    requirements: "",
+    dlc: false,
+    tip: ""
+  },
+  {
+    number: 3,
+    name: "Venusaur",
+    type: ["Grass", "Poison"],
+    game: "",
+    location: "",
+    habitat: "",
+    requirements: "",
+    dlc: false,
+    tip: ""
+  },
+  {
+    number: 4,
+    name: "Charmander",
+    type: ["Fire"],
+    game: "",
+    location: "",
+    habitat: "",
+    requirements: "",
+    dlc: false,
+    tip: ""
+  },
+  {
+    number: 5,
+    name: "Charmeleon",
+    type: ["Fire"],
+    game: "",
+    location: "",
+    habitat: "",
+    requirements: "",
+    dlc: false,
+    tip: ""
+  },
+  {
+    number: 6,
+    name: "Charizard",
+    type: ["Fire", "Flying"],
+    game: "",
+    location: "",
+    habitat: "",
+    requirements: "",
+    dlc: false,
+    tip: ""
+  },
+  {
+    number: 7,
+    name: "Squirtle",
+    type: ["Water"],
+    game: "",
+    location: "",
+    habitat: "",
+    requirements: "",
+    dlc: false,
+    tip: ""
+  },
+  {
+    number: 8,
+    name: "Wartortle",
+    type: ["Water"],
+    game: "",
+    location: "",
+    habitat: "",
+    requirements: "",
+    dlc: false,
+    tip: ""
+  },
+  {
+    number: 9,
+    name: "Blastoise",
+    type: ["Water"],
+    game: "",
+    location: "",
+    habitat: "",
+    requirements: "",
+    dlc: false,
+    tip: ""
+  },
+  {
+    number: 10,
+    name: "Caterpie",
+    type: ["Bug"],
+    game: "",
+    location: "",
+    habitat: "",
+    requirements: "",
+    dlc: false,
+    tip: ""
   }
 ];

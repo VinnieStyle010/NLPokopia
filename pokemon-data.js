3435c0e6183d4707942563213a26ea1baa3b1308
VinnieStyle010 <v.a.j.janse@gmail.com>
2026-08-20 18:04:26 +0200

    Update pokemon-data.js

--- pokemon-data.js
+++ pokemon-data.js
@@ -1,1 +1,20 @@
-
+const pokemonData = [
+  {
+    number: 1,
+    name: "Bulbasaur",
+    type: ["Grass", "Poison"],
+    game: "Base Game",
+    location: "",
+    habitat: "",
+    requirements: "",
+    dlc: false,
+    tip: "",
+    videos: {
+      youtube: "",
+      tiktok: "",
+      instagram: "",
+      facebook: "",
+      twitter: ""
+    }
+  }
+];

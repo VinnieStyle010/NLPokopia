60b929150038dd6143bdf6ea73a01a5fc5240c16
VinnieStyle010 <v.a.j.janse@gmail.com>
2026-08-20 21:02:20 +0200

    Update pokemon-data.js

--- pokemon-data.js
+++ pokemon-data.js
@@ -1,5 +1,31 @@
+function normalizePokemon(pokemon) {
+    return {
+        number: pokemon.number,
+        name: pokemon.name,
+        type: pokemon.type || [],
+
+        game: pokemon.game ?? "",
+        dlc: pokemon.dlc ?? null,
+
+        location: pokemon.location ?? "",
+        area: pokemon.area ?? "",
+        habitat: pokemon.habitat ?? "",
+
+        obtainMethod: pokemon.obtainMethod ?? "",
+        requirements: pokemon.requirements ?? "",
+
+        evolvesFrom: pokemon.evolvesFrom ?? "",
+        evolvesInto: pokemon.evolvesInto ?? "",
+        evolutionMethod: pokemon.evolutionMethod ?? "",
+
+        tip: pokemon.tip ?? ""
+    };
+}
+
 const pokemonData = [
-  ...pokemonKanto,
-  ...pokemonExtra,
-  ...pokemonDLC
-];
+    ...pokemonKanto,
+    ...pokemonExtra,
+    ...pokemonDLC
+]
+.map(normalizePokemon)
+.sort((a, b) => a.number - b.number);

c3174ec371a7655a1eb80ec904c3c71b0472cb01
VinnieStyle010 <v.a.j.janse@gmail.com>
2026-08-25 15:28:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -107,9 +107,6 @@ function renderPokemon(list) {
                 loading="lazy"
             >
 
-            <<h2>
-    NR: ${String(pokemon.number).padStart(3, "0")} ${pokemon.name}
-</h2>
 
             <p class="pokemon-card-types">
                 <strong>Type:</strong>

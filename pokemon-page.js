90835c8a3d7326a48654444d6779467eb9d1388d
VinnieStyle010 <v.a.j.janse@gmail.com>
2026-08-27 15:35:43 +0200

    Update pokemon-page.js

--- pokemon-page.js
+++ pokemon-page.js
@@ -3,7 +3,7 @@ const detailContainer = document.getElementById("pokemonDetail");
 function getPokemonImage(name) {
 
     const imageNames = {
-        "Paldean Wooper": "wooper-paldea",
+        "Paldean Wooper": "wooper-paldean",
 
         "Heat Rotom": "rotom-heat",
         "Wash Rotom": "rotom-wash",

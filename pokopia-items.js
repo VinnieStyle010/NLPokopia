183d200c141d9e41b187aac1d74ecc99bae87a2b
VinnieStyle010 <v.a.j.janse@gmail.com>
2026-08-29 21:41:52 +0200

    Update pokopia-items.js

--- pokopia-items.js
+++ pokopia-items.js
@@ -1,1 +1,11 @@
 
+// ========================================
+// NLPokopia - Pokopia itemdatabase
+// ========================================
+
+// Hier bewaren we alle echte Pokopia-items.
+// Elk item kan later centraal aan de juiste afbeelding worden gekoppeld.
+
+const pokopiaItems = {
+    
+};
